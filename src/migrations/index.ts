c2e898b914acfd9704ec00d6e4f730da0cfa62e5
MAPCMC <maartenadampeene@gmail.com>
2024-10-14 21:34:14 +0200

    Add initial migration files

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241014_193323 from './20241014_193323';
+
+export const migrations = [
+  {
+    up: migration_20241014_193323.up,
+    down: migration_20241014_193323.down,
+    name: '20241014_193323'
+  },
+];

080fa695635e5da42e4a71769b12cb8628065c17
MAPCMC <maartenadampeene@gmail.com>
2024-10-23 18:29:55 +0200

    Use components on homepage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,20 +3,13 @@ import config from "@payload-config";
 import { Triangle } from "@/app/(app)/_components/Triangle";
 import Image from "next/image";
 import { MainLogo } from "@/app/(app)/_components/MainLogo";
-import { CallToAction, Media, Text } from "@/payload-types";
 import Link from "next/link";
-import { Lexical } from "@/app/(app)/_components/Lexical";
 import { Button } from "@/app/(app)/_components/Button";
+import { isMedia } from "@/app/(app)/_helpers";
+import PageContent from "./_components/PageContent";
+import PageFooter from "./_components/PageFooter";
 
 const payload = await getPayloadHMR({ config });
-
-function isMedia(logo: any): logo is Media {
-  return (logo as Media).url !== undefined;
-}
-
-function isText(item: CallToAction | Text): item is Text {
-  return "text" in item;
-}
 
 export default async function Home() {
   const HomeData = await payload.findGlobal({
@@ -24,14 +17,11 @@ export default async function Home() {
     depth: 4,
   });
 
-  const headerBlock = HomeData.layout?.find(
-    (block) => block.blockType === "header"
-  );
+  const headerData = HomeData.header?.[0];
+  const footerData = await payload.findGlobal({
+    slug: "footer",
+  });
 
-  const defaultMenuItem = {
-    label: "terug naar boven",
-    link: "#body",
-  };
   const menuItems = HomeData.layout?.reduce(
     (acc, block) => {
       if (block.blockType === "section" && block.anchor) {
@@ -42,7 +32,7 @@ export default async function Home() {
       }
       return acc;
     },
-    [defaultMenuItem] as { label: string; link: string }[]
+    [] as { label: string; link: string }[]
   );
 
   // TODO add data pages & socials
@@ -54,8 +44,8 @@ export default async function Home() {
 
   return (
     <>
-      <div className="w-full overflow-hidden pb-[50%]">
-        <div className="max-w-5xl mx-auto px-4 pt-12 flex items-center justify-between">
+      <header className="w-full overflow-hidden pb-[50%] -mb-[50%]">
+        <div className="w-full sm:max-w-5xl sm:mx-auto px-4 pt-12 flex items-center justify-between">
           <div className="relative">
             <Triangle
               wrapperClassName="absolute -top-[66px] left-[-1307px] z-[-1] -translate-x-1/2 w-[3000px]"
@@ -64,7 +54,7 @@ export default async function Home() {
             <Link href="/">
               <MainLogo
                 position="header"
-                className="w-80"
+                className="w-40 sm:w-80"
               />
             </Link>
             <Triangle
@@ -72,198 +62,65 @@ export default async function Home() {
               className="bg-yellow-300/[16%] scale-y-[-1] rotate-90"
             />
           </div>
-          <nav className="flex gap-8">
+          <nav className="flex">
             {menuItems?.map((item, i) => {
-              // skip back to top default
-              if (i === 0) return null;
               return (
-                <Link
-                  key={i}
-                  href={item.link}
-                  className="h-small"
-                >
-                  {item.label}
-                </Link>
+                <Button asChild key={i} variant="link">
+                  <Link href={item.link}>
+                    <span>{item.label}</span>
+                  </Link>
+                </Button>
               );
             })}
           </nav>
         </div>
-      </div>
-      <div className="-mt-[50%]">
-        {!!headerBlock && (
-          <header
-            key={headerBlock.id}
-            className="overflow-hidden"
-          >
-            <div className="max-w-5xl mx-auto p-4 grid gap-4 grid-cols-[auto_1fr]">
-              {headerBlock.title && (
-                <h1 className="h-medium max-w-4xl col-span-full">
-                  {headerBlock.title}
-                </h1>
+        {!!headerData && (
+          <div className="max-w-5xl mx-auto p-4 grid gap-4 grid-cols-[auto_1fr]">
+            {headerData.title && (
+              <h1 className="h-medium max-w-4xl col-span-full">
+                {headerData.title}
+              </h1>
+            )}
+            {headerData.introduction && (
+              <p className="max-w-2xl h-small-light">
+                {headerData.introduction}
+              </p>
+            )}
+            {headerData.callToAction?.map((cta, i) => {
+              return (
+                <Button asChild key={i} shape="skewed">
+                  <Link href={cta.link}>
+                    <span>{cta.label}</span>
+                  </Link>
+                </Button>
+              );
+            })}
+            {headerData.image &&
+              isMedia(headerData.image) && (
+                <div className="col-start-2 row-start-2 row-span-2 flex">
+                  <Image
+                    src={headerData.image.url ?? ""}
+                    alt={headerData.image.alt ?? ""}
+                    width={headerData.image.width ?? 200}
+                    height={headerData.image.height ?? 200}
+                  />
+                  <Triangle
+                    wrapperClassName="rotate-90 -translate-x-[13.4%] w-[200px] shrink-0"
+                    className="bg-yellow-300/40"
+                  />
+                </div>
               )}
-              {headerBlock.introduction && (
-                <p className="max-w-2xl h-small-light">
-                  {headerBlock.introduction}
-                </p>
-              )}
-              {headerBlock.callToAction?.map((cta, i) => {
-                return (
-                  <Button asChild key={i}>
-                    <Link
-                      href={cta.link}
-                      className="col-start-1"
-                    >
-                      {cta.label}
-                    </Link>
-                  </Button>
-                );
-              })}
-              {headerBlock.image &&
-                isMedia(headerBlock.image) && (
-                  <div className="col-start-2 row-start-2 row-span-2 flex">
-                    <Image
-                      src={headerBlock.image.url ?? ""}
-                      alt={headerBlock.image.alt ?? ""}
-                      width={headerBlock.image.width ?? 200}
-                      height={
-                        headerBlock.image.height ?? 200
-                      }
-                    />
-                    <Triangle
-                      wrapperClassName="rotate-90 -translate-x-[13.4%] w-[200px] shrink-0"
-                      className="bg-yellow-300/40"
-                    />
-                  </div>
-                )}
-            </div>
-          </header>
-        )}
-        <main className="-mb-[80%] space-y-36">
-          {HomeData.layout?.map((block) => {
-            return (() => {
-              switch (block.blockType) {
-                case "header":
-                  return null;
-                case "section":
-                  const idProp = block.anchor
-                    ? { id: block.anchor }
-                    : {};
-                  return (
-                    <section
-                      {...idProp}
-                      key={block.id}
-                      className="max-w-5xl mx-auto p-4"
-                    >
-                      {block.title && (
-                        <h2 className="h-large">
-                          {block.title}
-                        </h2>
-                      )}
-                      {block.introduction && (
-                        <p className="h-small-light max-w-2xl">
-                          {block.introduction}
-                        </p>
-                      )}
-                      {!!block.content &&
-                        block.content.length > 0 &&
-                        block.content?.map(
-                          (blockContent, i) => {
-                            if (isText(blockContent))
-                              return (
-                                <Lexical
-                                  key={i}
-                                  content={
-                                    blockContent.text
-                                  }
-                                />
-                              );
-                            return null;
-                          }
-                        )}
-                    </section>
-                  );
-                default:
-                  console.error(`Unknown block type`);
-                  return null;
-              }
-            })();
-          })}
-        </main>
-        <footer className="w-full overflow-hidden pt-[80%] pb-36">
-          <div className="max-w-5xl mx-auto p-4 flex justify-between items-end pt-36">
-            <div>
-              <MainLogo
-                position="footer"
-                className="w-120 mb-16"
-              />
-              {HomeData.footerCopyright?.root !==
-                undefined && (
-                <Lexical
-                  content={HomeData.footerCopyright}
-                />
-              )}
-            </div>
-            <div className="relative">
-              <Triangle
-                wrapperClassName="absolute -bottom-40 -left-36 z-[-1] origin-bottom-left w-[2000px]"
-                className="bg-yellow-300 scale-y-[-1] rotate-180"
-              />
-              <nav className="min-w-80">
-                {/* main menu */}
-                <ul className="border border-foreground bg-background">
-                  {menuItems?.map((item, i) => {
-                    return (
-                      <li key={i}>
-                        <Link
-                          className="block p-3 hover:bg-foreground hover:text-background first-letter:capitalize"
-                          href={item.link}
-                        >
-                          {item.label}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-                {/* dynamic pages */}
-                {dynamicPages.length > 0 && (
-                  <ul className="border border-foreground bg-background">
-                    {dynamicPages?.map((item, i) => {
-                      return (
-                        <li
-                          key={i}
-                          className="p-3 hover:bg-foreground hover:text-background"
-                        >
-                          <a href={item.link}>
-                            {item.label}
-                          </a>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                )}
-                {/* social links */}
-                {socials?.length > 0 && (
-                  <ul className="border border-foreground bg-background">
-                    {socials?.map((item, i) => {
-                      return (
-                        <li key={i} className="">
-                          <a
-                            target="_blank"
-                            href={item.link}
-                            className="p-3 hover:bg-foreground hover:text-background"
-                          >
-                            {item.label}
-                          </a>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                )}
-              </nav>
-            </div>
           </div>
-        </footer>
-      </div>
+        )}
+      </header>
+
+      <PageContent blocks={HomeData.layout} />
+      <PageFooter
+        anchors={menuItems}
+        pages={dynamicPages}
+        socials={socials}
+        content={footerData?.copyright}
+      />
     </>
   );
 }

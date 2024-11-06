da56258f625877be295b7de86ad210e70f498dc6
MAPCMC <maartenadampeene@gmail.com>
2024-11-06 01:11:33 +0100

    Add current migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241106_001108 from './20241106_001108';
+
+export const migrations = [
+  {
+    up: migration_20241106_001108.up,
+    down: migration_20241106_001108.down,
+    name: '20241106_001108'
+  },
+];

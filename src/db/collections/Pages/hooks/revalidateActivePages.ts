1f5094ed99cbe98eca9cd94b464328a45926b762
MAPCMC <maartenadampeene@gmail.com>
2024-11-18 21:14:27 +0100

    Fix: generate preview route & add view

--- src/db/collections/Pages/hooks/revalidateActivePages.ts
+++ src/db/collections/Pages/hooks/revalidateActivePages.ts
@@ -5,10 +5,13 @@ import { revalidateTag } from "next/cache";
 
 export const revalidateActivePages: CollectionAfterChangeHook<
   Page
-> = ({ doc, req: { payload } }) => {
-  payload.logger.info(`Revalidating active pages`);
-
-  revalidateTag("pages_active");
-
+> = ({ doc, previousDoc, req: { payload } }) => {
+  if (
+    doc._status === "published" ||
+    previousDoc?._status === "published"
+  ) {
+    payload.logger.info(`Revalidating active pages`);
+    revalidateTag("pages_active");
+  }
   return doc;
 };

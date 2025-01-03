5b20ef31bbaea78a9da584c27d670015fd935710
MAPCMC <maartenadampeene@gmail.com>
2025-01-03 18:27:48 +0100

    Add appearance of contact on service selection with details summary

--- src/app/(app)/_components/Services/Client.tsx
+++ src/app/(app)/_components/Services/Client.tsx
@@ -8,6 +8,7 @@ import type { Filter, Service, ServiceSection } from "@/payload-types";
 import { useSearchParams, useRouter } from "next/navigation";
 import { cn } from "../../_helpers";
 import { requiredPath } from "./requiredPath";
+import { Triangle } from "../Triangle";
 
 interface ServicesProps {
   filters: Filter[];
@@ -20,6 +21,7 @@ export const ServicesClient: React.FC<ServicesProps> = ({
   services,
   section,
 }) => {
+  const [selectedServices, setSelectedServices] = React.useState<number[]>([]);
   const searchParams = useSearchParams();
   const router = useRouter();
 
@@ -80,6 +82,8 @@ export const ServicesClient: React.FC<ServicesProps> = ({
   const handleFilterClick = (key: string, value: string, multiple: boolean) => {
     const newSearchParams = new URLSearchParams(searchParams);
 
+    setSelectedServices([]);
+
     if (newSearchParams.has(key, value)) {
       newSearchParams.delete(key, value);
     } else {
@@ -110,109 +114,183 @@ export const ServicesClient: React.FC<ServicesProps> = ({
     });
   };
 
+  const handleServiceClick = (serviceId: number) => {
+    const newSelectedServices = selectedServices.includes(serviceId)
+      ? selectedServices.filter((id) => id !== serviceId)
+      : [...selectedServices, serviceId];
+    setSelectedServices(newSelectedServices);
+  };
+
   return (
     <>
-      {filters.map((filter) => {
-        return (
-          <div
-            key={filter.id}
-            className={cn(
-              "my-8 flex h-auto flex-wrap items-center justify-center gap-2 transition-all",
-              {
-                "hidden h-0": !visibleFilters.find(
-                  (vis) => vis.id === filter.id,
-                ),
-              },
-            )}
-          >
-            {filter.options?.map((option, fi) => {
-              const isSelected = (key: string, value: string) => {
-                return searchParams?.has(key, value);
-              };
-              return (
-                <div key={fi}>
-                  <Button
-                    variant={
-                      isSelected(filter.key, option.value)
-                        ? "selected"
-                        : "outline"
-                    }
-                    key={fi}
-                    shape="skewed"
-                    onClick={() =>
-                      handleFilterClick(
-                        filter.key,
-                        option.value,
-                        filter.multiple,
-                      )
-                    }
-                  >
-                    <span className="block">{option.label}</span>
-                  </Button>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
-      {filteredServices.length > 3 && (
-        <p className="mx-auto max-w-5xl px-4 text-center">
-          Ik heb meer informatie nodig
-        </p>
-      )}
-      {filteredServices.length === 0 && (
-        <div className="mx-auto max-w-5xl space-y-3 px-4 text-center">
-          <p className="h-small">
-            {"Oeps! geen scenario's met (al) deze criteria."}
-          </p>
-        </div>
-      )}
-      {searchParams.size > 0 && (
-        <div
-          className={cn(
-            "mx-auto mb-6 max-w-5xl px-4 text-right",
-            filteredServices.length === 0 ? "text-center" : "",
-          )}
-        >
+      <div className="relative">
+        <div className="mx-auto mb-6 h-10 max-w-5xl px-4 text-right">
           <Button
-            onClick={() => router.push("?", { scroll: false })}
+            onClick={() => {
+              setSelectedServices([]);
+              router.push("?", { scroll: false });
+            }}
             variant="outline"
             shape="small"
+            className={searchParams.size > 0 ? "inline" : "hidden"}
           >
             Filters verwijderen
           </Button>
         </div>
-      )}
-      {filteredServices.length > 0 && filteredServices.length <= 3 && (
-        <>
-          {section.resultsIntro && (
-            <div className="mx-auto max-w-5xl px-4">
-              <div>
-                <h2 className="h-small">
-                  {`Relevante scenario's (${filteredServices.length})`}
-                </h2>
-                <Lexical content={section.resultsIntro} />
-              </div>
+        <Triangle
+          orientation="down"
+          className="bg-red-light"
+          wrapperClassName="absolute -top-64 h-[calc(100%+26rem)] left-1/2 -translate-x-1/2 z-[-1]"
+        />
+        {filters.map((filter, i) => {
+          return (
+            <div
+              key={filter.id}
+              className={cn(
+                "mb-6 flex h-auto flex-wrap items-center justify-center gap-2",
+                {
+                  "hidden h-0": !visibleFilters.find(
+                    (vis) => vis.id === filter.id,
+                  ),
+                  "animate-in slide-in-from-bottom-8": visibleFilters.find(
+                    (vis) => vis.id === filter.id,
+                  ),
+                },
+              )}
+            >
+              {filter.options?.map((option, fi) => {
+                const isSelected = (key: string, value: string) => {
+                  return searchParams?.has(key, value);
+                };
+                return (
+                  <div key={fi}>
+                    <Button
+                      variant={
+                        isSelected(filter.key, option.value)
+                          ? "selected"
+                          : "outline"
+                      }
+                      key={fi}
+                      shape="skewed"
+                      onClick={() =>
+                        handleFilterClick(
+                          filter.key,
+                          option.value,
+                          filter.multiple,
+                        )
+                      }
+                    >
+                      <span className="block">{option.label}</span>
+                    </Button>
+                  </div>
+                );
+              })}
             </div>
-          )}
-          <div className="mx-auto grid max-w-5xl grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3">
-            {filteredServices.map((service, i) => {
-              return (
-                <article
-                  className="relative row-span-2 grid grid-rows-subgrid border border-foreground bg-background p-4"
-                  key={i}
-                >
-                  <h3 className="h-small">{service.title}</h3>
-                  {service.content && (
-                    <div>
-                      <Lexical content={service.content} />
-                    </div>
-                  )}
-                </article>
-              );
-            })}
+          );
+        })}
+        {filteredServices.length > 3 && (
+          <p className="mx-auto max-w-5xl px-4 text-center">
+            Selecteer meer velden voor gerichte informatie
+          </p>
+        )}
+        {filteredServices.length === 0 && (
+          <div className="mx-auto max-w-5xl space-y-3 px-4 text-center animate-in slide-in-from-bottom-8">
+            <p className="h-small">
+              {"Oeps! geen scenario's met (al) deze criteria."}
+            </p>
           </div>
-        </>
+        )}
+        {filteredServices.length > 0 && filteredServices.length <= 3 && (
+          <div className="mb-12 animate-in slide-in-from-bottom-8">
+            {section.resultsIntro && (
+              <div className="mx-auto max-w-5xl px-4">
+                <div>
+                  <h2 className="h-small">
+                    {`Relevante scenario's (${filteredServices.length})`}
+                  </h2>
+                  <Lexical content={section.resultsIntro} />
+                </div>
+              </div>
+            )}
+            <div className="mx-auto grid max-w-5xl grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3">
+              {filteredServices.map((service, i) => {
+                return (
+                  <article
+                    className="relative row-span-3 grid grid-rows-subgrid border border-foreground bg-background p-4"
+                    key={i}
+                  >
+                    <h3 className="h-small">{service.title}</h3>
+                    {service.content && (
+                      <div>
+                        <Lexical content={service.content} />
+                      </div>
+                    )}
+                    <Button
+                      variant={
+                        selectedServices.includes(service.id)
+                          ? "selected"
+                          : "outline"
+                      }
+                      shape="skewed"
+                      className="mr-8"
+                      onClick={() => handleServiceClick(service.id)}
+                    >
+                      <span>Dit zoek ik</span>
+                    </Button>
+                  </article>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+      {selectedServices.length > 0 && (
+        <section className="bg-red animate-in slide-in-from-bottom-8">
+          <div className="mx-auto max-w-5xl px-4 py-16">
+            <h2 className="h-large">Gericht contact</h2>
+            <div className="grid gap-3 md:grid-cols-3 md:gap-6">
+              <div className="lexical border border-foreground bg-background p-4 md:col-start-3">
+                <h3>Wat we weten</h3>
+                <p>Over jou/jullie:</p>
+                <ul>
+                  {Object.keys(entries).map((key: string, i: number) => {
+                    console.log(key);
+                    const filter = visibleFilters.find((filter) => {
+                      return filter.key === key;
+                    });
+                    const options = filter?.options
+                      ?.filter((option) => {
+                        return searchParams.has(key, option.value);
+                      })
+                      .map((option) => option.label);
+
+                    return (
+                      <li
+                        key={`${key}-${i}`}
+                        className="list-inside list-[square]"
+                      >
+                        {options?.join("/")}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <p>Je kan hulp gebruiken bij:</p>
+                <ul className="list-inside list-[square]">
+                  {services
+                    .filter((service) => {
+                      return selectedServices.includes(service.id);
+                    })
+                    .map((service, i) => {
+                      return <li key={i}>{service.title}</li>;
+                    })}
+                </ul>
+              </div>
+              <form className="border border-foreground bg-background p-4 md:col-span-2 md:row-start-1">
+                <h3 className="h-small">Navragen beschikbaarheid</h3>
+              </form>
+            </div>
+          </div>
+        </section>
       )}
     </>
   );

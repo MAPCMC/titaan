622b1167c28e230dd82ab7a8813d32c2662969b7
MAPCMC <maartenadampeene@gmail.com>
2025-01-29 13:03:40 +0100

    intermezzo: integrate RichText into Lexical

--- src/app/(app)/_components/Lexical/index.tsx
+++ src/app/(app)/_components/Lexical/index.tsx
@@ -1,18 +1,24 @@
 import "./index.css";
 import { cn } from "../../_helpers";
-import { RichText } from "../RichText";
+import { serializeLexical } from "./serialize";
 
 export function Lexical({
   content,
   className,
+  excerpt = false,
 }: {
   content?: any;
   className?: string;
+  excerpt?: boolean;
 }) {
   if (!content) return null;
   return (
     <div className={cn("lexical mt-4 max-w-2xl", className)}>
-      {content && <RichText content={content} excerpt={false} />}
+      {content &&
+        !Array.isArray(content) &&
+        typeof content === "object" &&
+        "root" in content &&
+        serializeLexical({ nodes: content?.root?.children, excerpt })}
     </div>
   );
 }

accf7392b0ec15a1b4e9a41b560174899a925692
MAPCMC <maartenadampeene@gmail.com>
2025-01-30 19:51:22 +0100

    add migration

--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241106_110503 from './20241106_110503';
 import * as migration_20241210_123024 from './20241210_123024';
 import * as migration_20250128_120859 from './20250128_120859';
+import * as migration_20250130_185104 from './20250130_185104';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20250128_120859.up,
     down: migration_20250128_120859.down,
-    name: '20250128_120859'
+    name: '20250128_120859',
+  },
+  {
+    up: migration_20250130_185104.up,
+    down: migration_20250130_185104.down,
+    name: '20250130_185104'
   },
 ];
